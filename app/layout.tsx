67d975280c9fc61a055add5590ced1616106f50c
sarang <sarang.office01@gmail.com>
2024-09-19 13:07:12 +0530

    footer sec added

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+export const metadata: Metadata = {
+  title: "Travel App",
+  description: "Lets Travel Across",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main className="relative overflow-hidden">
+          {children}
+          </main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
